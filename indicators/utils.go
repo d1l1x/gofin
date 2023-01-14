d08b451fbe0629566c531421ebda023da3ff831b
d1l1x <115579755+d1l1x@users.noreply.github.com>
2023-01-14 15:08:32 +0100

    Add StdDev (#5)

--- indicators/utils.go
+++ indicators/utils.go
@@ -1,9 +1,22 @@
 package indicators
 
+import (
+	"math"
+)
+
 func Mean(in []float64) float64 {
 	res := 0.0
 	for _, val := range in {
 		res += val
 	}
 	return res/float64(len(in))
+}
+
+func StdDev(in []float64) float64 {
+	mu := Mean(in)
+	res := 0.0
+	for _, val := range in {
+		res += math.Pow(val - mu, 2)
+	}
+	return math.Sqrt(res/float64(len(in)))
 }
