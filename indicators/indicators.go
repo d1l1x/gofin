8b680d41b7cd92deee9149c4376bcddb734cc149
dilix <115579755+d1l1x@users.noreply.github.com>
2023-06-19 19:07:00 +0200

    Restructure RSI

--- indicators/indicators.go
+++ indicators/indicators.go
@@ -7,3 +7,21 @@ type BarHistory struct {
 	Close  []float64
 	Volume []int64
 }
+
+type Indicator struct {
+	input []float64
+	values []float64
+	period int
+}
+
+func (i *Indicator) Last() float64 {
+	return i.values[len(i.values)-1]
+}
+
+func NewIndicator(input []float64, period int) Indicator {
+	return Indicator{
+		input: input, // or some other initial values
+		values: []float64{}, // or some other initial values
+		period: period,
+	}
+}
--- indicators/relative_strength_index.go
+++ indicators/relative_strength_index.go
@@ -1,38 +1,49 @@
 package indicators
 
+func RSI(input []float64, period int) *RelativeStrengthIndex {
+	return &RelativeStrengthIndex{
+		Indicator: NewIndicator(input,period),
+	}
+}
+
+type RelativeStrengthIndex struct {
+	Indicator
+}
+
 //Calculate Relative Strength Index (RSI)
-func RSI(input []float64, period int) []float64 {
-	if len(input) < period {
+func (ind *RelativeStrengthIndex) Compute() []float64 {
+	if len(ind.input) < ind.period {
 		return nil
 	}
 
-	rsi := make([]float64, len(input))
+	rsi := make([]float64, len(ind.input))
 
 	sumGains := 0.0
 	sumLosses := 0.0
 
-	for i:=1; i<period; i++ {
-		gain, loss := calculateGainLoss(input[i], input[i-1])
+	for i:=1; i<ind.period; i++ {
+		gain, loss := ind.calculateGainLoss(ind.input[i], ind.input[i-1])
 		sumGains += gain
 		sumLosses += loss
 	}
-	sumGains /= float64(period)
-	sumLosses /= float64(period)
+	sumGains /= float64(ind.period)
+	sumLosses /= float64(ind.period)
 
-	rsi[period-1] = 100.0 - ( 100.0 / ( 1.0 + sumGains / sumLosses ) )
+	rsi[ind.period-1] = 100.0 - ( 100.0 / ( 1.0 + sumGains / sumLosses ) )
 
-	for i:=period; i<len(input); i++ {
-		gain, loss := calculateGainLoss(input[i], input[i-1])
-		sumGains = (sumGains * float64(period-1)  + gain) / float64(period)
-		sumLosses = (sumLosses * float64(period-1)  + loss) / float64(period)
+	for i:=ind.period; i<len(ind.input); i++ {
+		gain, loss := ind.calculateGainLoss(ind.input[i], ind.input[i-1])
+		sumGains = (sumGains * float64(ind.period-1)  + gain) / float64(ind.period)
+		sumLosses = (sumLosses * float64(ind.period-1)  + loss) / float64(ind.period)
 		rsi[i] = 100.0 - ( 100.0 / ( 1.0 + sumGains / sumLosses ) )
 	}
 	// first value is just to start the averaging
-	rsi[period-1] = 0.0
+	rsi[ind.period-1] = 0.0
+	ind.values = rsi
 	return rsi
 }
 
-func calculateGainLoss(currentPrice, previousPrice float64) (float64, float64) {
+func (ind *RelativeStrengthIndex) calculateGainLoss(currentPrice, previousPrice float64) (float64, float64) {
 	gain := 0.0
 	loss := 0.0
 	if currentPrice > previousPrice {
